7aa1274fd579f61ffe8498153dcd4332d90c4179
Bruno Simplicio <brunoraphael89@gmail.com>
2016-06-22 00:05:06 -0300

    Add Project and Task Controllers/Views

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile'},
              controllers: {omniauth_callbacks: "omniauth_callbacks"}
 
-  root "pages#about"
+  root "project#index"
   get 'pages/about'
+
+  resources :project do
+    resources :task, only: [:show]
+  end
 
 end
