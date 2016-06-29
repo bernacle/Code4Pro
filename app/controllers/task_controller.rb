69245e77d16a416d7b890f51e53b5b130bda65fc
Bruno Simplicio <brunoraphael89@gmail.com>
2016-06-29 20:26:17 -0300

    Add validation to Task Controller

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,10 +1,27 @@
 class TaskController < ApplicationController
+
+  before_action :authenticate_user!
+
   def show
     project = Project.find(params[:project_id])
     @tasks = project.tasks.order(:tag)
-    @task = @tasks.find(params[:id])
 
-    @next_task = @task.next
-    @prev_task = @task.prev
+
+    joined = false
+
+    if current_user && current_user.projects
+      joined = current_user.projects.include?(project) #include? veryfies if that specific project is on the projects of current user
+    end
+
+    if joined
+      @task = @tasks.find(params[:id])
+
+      @next_task = @task.next
+      @prev_task = @task.prev
+    else
+      flash[:notice] = "No permission to this!"
+      redirect_to project
+    end
+
   end
 end
