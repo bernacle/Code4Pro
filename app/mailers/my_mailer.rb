2eef1fde2b8e60a4d15383da1efaf4652a227f8f
Bruno Simplicio <brunoraphael89@gmail.com>
2016-07-04 21:40:51 -0300

    Add mailer to send payment receipt to user

--- app/mailers/my_mailer.rb
+++ app/mailers/my_mailer.rb
@@ -6,4 +6,10 @@ class MyMailer < ActionMailer::Base
     mail( :to => @user.email,
     :subject => 'Thanks for signing up for our amazing app' )
   end
+
+  def new_receipt(user)
+    @user = user
+    mail( :to => @user.email,
+    :subject => 'Your purchase on our website went great! Log in to see the details!' )
+  end
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,13 @@
 class Subscription < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
+
+  after_create :send_receipt_email
+
+  private
+    def send_receipt_email
+      @user = User.find_by_email(self.user.email)
+
+      MyMailer.new_receipt(@user).deliver
+    end
 end
